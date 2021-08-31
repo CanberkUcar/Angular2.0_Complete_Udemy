316d3cdafe568ac365743ef0fe6f03d7643e95ea
Janberk Uçar <canberk.ucar@revolvia.com>
2021-08-31 12:23:21 +0300

    Added notes for future.
    Explanation with Udemy

--- gettingStarted/ng-test/src/app/app.component.ts
+++ gettingStarted/ng-test/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+/**
+ * TODO:(TS Changes here.) You can edit this variable to
+ * use anywhere in the module.
+ */
 export class AppComponent {
   title = 'ng-test';
 }
--- gettingStarted/ng-test/src/app/app.module.ts
+++ gettingStarted/ng-test/src/app/app.module.ts
@@ -1,9 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Input } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 /* ---------- Added Modules --------- */
+/**
+ * TODO:(Module Changes here.) Added FormsModule to
+ *  bind ngModel to input, this is the way on Angular.
+ * ! Dont forget to add Bootstrap (if you are using)
+ * to angular.json as part of the setup too.
+ * That part explained on GettingStarted 12.
+ */
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
